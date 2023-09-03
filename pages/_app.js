88001d098a22476cb4e16ce8a6f18f374cf2eab2
Gias uddin vuiya <giasuddinvuiya2022@gmail.com>
2023-09-03 13:07:01 +0600

    Navber-and-component-added

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
+import Nav from '../components/Nav'
 
 function MyApp({ Component, pageProps }) {
   return (
+    <>
+    <Nav />
     <Layout>
        <Component {...pageProps} />
     </Layout>
+    </>
   )
 }
 
